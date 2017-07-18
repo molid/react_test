8328634ddf39ca053e30203263b72270611f64d8
unknown <molid@TWA2BAHXUA.asiapacific.hpqcorp.net>
2017-07-18 19:29:20 +0800

    11

--- index.jsx
+++ index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+// import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import {render} from 'react-dom';
 import { createStore,bindActionCreators } from 'redux';
 import { Provider ,connect} from 'react-redux';
@@ -27,8 +28,9 @@ import AccordionPanel from 'grommet/components/AccordionPanel';
 import SocialShare from 'grommet/components/SocialShare';
 import Animate from 'grommet/components/Animate';
  import Value from 'grommet/components/Value';
-
-
+import LoginForm from 'grommet/components/LoginForm';
+import Logo from './Logo';
+import Promo from './Promo';
 
 
 class SearchT extends React.Component{
@@ -77,151 +79,39 @@ class SearchT extends React.Component{
         // var myBoolean=new Boolean();
         var myBoolean1=this.state.myBoolean;
         var mm=this.state.Animate_Bool;
+        const { session } = this.props;
+
         return (
-            <Split flex='right'>
-            <div>
-                <Sidebar colorIndex='neutral-1'
-                         fixed={true}
-                         full={true}>
-                    <Header pad='medium'
-                            justify='between'>
-                        <Title>
-                            Title
-                        </Title>
-                    </Header>
-                    <Box flex='grow'
-                         justify='start'>
-                        <Menu primary={true}>
-                            <Anchor href='#'
-                                    className='active'>
-                                First
-                            </Anchor>
-                            <Anchor href='#'>
-                                Second
-                            </Anchor>
-                            <Anchor href='#'>
-                                Third
-                            </Anchor>
-                            <Anchor href='#'>
-                                Third
-                            </Anchor>
-                        </Menu>
-                    </Box>
-                    <Footer pad='medium'>
-                        <Button icon={<User />} />
+            <Split flex="left" separator={true}>
+                <Promo />
+                <Sidebar justify="between" align="center" pad="none" size="large">
+                    <span />
+                    <LoginForm align="start"
+                               logo={<Logo size="large" busy={this.state.busy} />}
+                               title={this.props.productName}
+                               onSubmit={this.state.busy ? null : this._onSubmit}
+
+                               usernameType="text" />
+                    <Footer direction="row" size="small"
+                            pad={{horizontal: 'medium', vertical: 'small', between: 'small'}}>
+                        <span className="secondary">&copy; 2016 Grommet Labs</span>
                     </Footer>
                 </Sidebar>
-            </div>
-            <div>
-                {/*<Layer closer={true} flush={true} hidden="true">*/}
-                <Section  align="center" styles='display:none' visible={false}>
-                    <h1 strong={true} visible={false}>
-                        {searchValue}
-                    </h1>
-                    <br/>
-                    <Search placeHolder='Search'
-                            inline={true}
-                            visible={false}
-                            // value={searchValue}
-                            dropAlign={{"right": "right"}}
-                            onDOMChange={(event) => {
-                                this.onchange(event)
-                            }} />
-                    <br/>
-                    <Button icon={<EditIcon />}
-                            label='点击'
-                            onClick={(event) =>this.changeText()}
-                            href='#'/>
-                    <br/>
-                    <Button icon={<EditIcon />}
-                            label='点击跳转'
-                            onClick={(event)=>this.ccc()}
-                            href='#'/>
-                    <Accordion openMulti={false}>
-                        <AccordionPanel heading='First Title'>
-                            <Paragraph>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            </Paragraph>
-                        </AccordionPanel>
-                        <AccordionPanel heading='Second Title'>
-                            <Paragraph>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            </Paragraph>
-                        </AccordionPanel>
-                        <AccordionPanel heading='Third Title'>
-                            <Paragraph>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            </Paragraph>
-                        </AccordionPanel>
-                    </Accordion>
-                    <Box pad={{"between": "medium"}}
-                         align='center'>
-
-                        <Button id ="but" label={this.state.label}
-                                primary={true}
-                        onClick={this.aaa}/>
-                        <Animate   ref="aa" visible={mm} enter={{"animation": "fade", "duration": 1000, "delay": 0}}
-                                 keep={true}>
-                            <Box direction='row'>
-                                <Box colorIndex='light-2'
-                                     margin='medium'
-                                     pad='large'>
-                                    <Value value={1} />
-                                </Box>
-                            </Box>
-                        </Animate>
-                    </Box>
-                </Section>
-                    <Layer dispaly = "none" closer={true} flush={true} hidden={myBoolean1}>
-                    <Button icon={<EditIcon />}
-                            label='关闭'
-                            onClick={(event) =>this.ccc()}
-                            href='#'/>
-                    <br/>
-                    <Card thumbnail='http://img.mp.itc.cn/upload/20170524/3dd5ba0bb91d4ef4acb2cfb4ed8c8fab_th.jpg'
-                              label='LeBron Raymone James'
-                              heading='LeBron Raymone James'
-                              description='LeBron Raymone James description providing more details.'/>
-
-                        <Footer justify='between'>
-                            {/*<Title>*/}
-                                {/*/!*<Logo />*!/*/}
-                                {/*Title*/}
-                            {/*</Title>*/}
-                            <SocialShare type='twitter'
-                                         link='https://grommet.io'
-                                         text='Sample text' />
-                            <Box direction='row'
-                                 align='center'
-                                 pad={{"between": "medium"}}>
-                                <Paragraph margin='none'>
-                                    © 2017 Molid Labs
-                                </Paragraph>
-                                <Menu direction='row'
-                                      size='small'
-                                      dropAlign={{"right": "right"}}>
-                                    <Anchor href='https://grommet.io'  target='https://grommet.io'>
-                                        Support
-                                    </Anchor>
-                                    <Anchor href='#'>
-                                        Contact
-                                    </Anchor>
-                                    <Anchor href='#'>
-                                        About
-                                    </Anchor>
-                                </Menu>
-                            </Box>
-                        </Footer>
-                    </Layer>
-
-            </div>
-
             </Split>
         );
     }
 }
+// SearchT.propTypes = {
+//     session: PropTypes.shape({
+//         error: PropTypes.string
+//     })
+// };
 
-
+// let select = (state) => ({
+//     session: state.session
+// });
+//
+// export default connect(select)(SearchT);
 
 render(
         <SearchT />,
